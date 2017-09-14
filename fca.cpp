df7227105710ef18241ccac04eb32b4d7dba7a24
rju <rju@purdue.edu>
2017-09-14 02:36:24 -0400

    Commit by Richard

--- fca.cpp
+++ fca.cpp
@@ -8,14 +8,16 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
 	string meal;
-	double cost = 3.14;
+	string scost;
+	double cost;
 	
 	cout << "This is fca.cpp" << endl;	
 	cout << "Enter what food item you ate just now" << endl;
 	getline(cin, meal);
 
-//	cout << "How much did this cost?" << endl;
-//	getline(cin, cost);
+	cout << "How much did this cost?" << endl;
+//	getline(cin, scost);
+	cin >> cost;
 
 	cout << "You ate " << meal << " for $" << cost << endl;
 
